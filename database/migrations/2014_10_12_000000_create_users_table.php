715396fcd704e1d550ce4fc4a781e1017ed80f30
AndresDiazGualteros <luisandresdiazgualteros22@gmail.com>
2022-12-12 10:39:08 -0500

    tablas

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
+            //tablas
             $table->id();
             $table->string('username');
             $table->string('email');
